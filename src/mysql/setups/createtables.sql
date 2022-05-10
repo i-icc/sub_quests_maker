d52246fb107107b4f9d46dd0a44059fb5eb4c0dc
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-05-10 16:09:10 +0900

    created table

--- src/mysql/setups/createtables.sql
+++ src/mysql/setups/createtables.sql
@@ -1,43 +1,64 @@
 CREATE TABLE user (
-    id int NOT NULL PRIMARY KEY,
-    uid varchar NOT NULL,
-    nickname varchar(50),
+    id int NOT NULL AUTO_INCREMENT,
+    uid char NOT NULL UNIQUE,
+    nickname char(50),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE who (
-    id int NOT NULL,
-    order varchar(100) NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    instruction char(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE whene (
-    id int NOT NULL,
-    order varchar(100) NOT NULL,
+CREATE TABLE timing (
+    id int NOT NULL AUTO_INCREMENT,
+    instruction char(100) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE place (
-    id int NOT NULL,
-    order varchar(100) NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    instruction char(100) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE what (
-    id int NOT NULL,
-    order varchar(100) NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    instruction char(100) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE quest (
-    id int NOT NULL,
-    user_uid varchar NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    user_uid char NOT NULL,
     who_id int,
-    whene_id int,
+    timing_id int,
     place_id int,
     what_id int NOT NULL,
-    comment varchar(200)
+    comment char(200),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_uid) REFERENCES user(uid) ON DELETE CASCADE,
+    FOREIGN KEY (who_id) REFERENCES who(id) ON DELETE CASCADE,
+    FOREIGN KEY (timing_id) REFERENCES timing(id) ON DELETE CASCADE,
+    FOREIGN KEY (place_id) REFERENCES place(id) ON DELETE CASCADE,
+    FOREIGN KEY (what_id) REFERENCES what(id) ON DELETE CASCADE
 );
 
 CREATE TABLE comment (
-    id int NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    user_uid char NOT NULL,
     quest_id int NOT NULL,
-    comment varchar(200) NOT NULL
+    comment char(200) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_uid) REFERENCES user(uid) ON DELETE CASCADE,
+    FOREIGN KEY (quest_id) REFERENCES quest(id) ON DELETE CASCADE
 );
 
-CREATE TABLE nice (user_uid varchar, quest_id int,);
+CREATE TABLE nice (
+    user_uid char NOT NULL,
+    quest_id int NOT NULL,
+    PRIMARY KEY (user_uid, quest_id),
+    FOREIGN KEY (user_uid) REFERENCES user(uid) ON DELETE CASCADE,
+    FOREIGN KEY (quest_id) REFERENCES quest(id) ON DELETE CASCADE
+);
