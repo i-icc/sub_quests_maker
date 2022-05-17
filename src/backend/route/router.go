5efba71d8c9d26c420b63f235715ab6dd836312d
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-05-17 15:23:23 +0900

    add rout

--- src/backend/route/router.go
+++ src/backend/route/router.go
@@ -13,6 +13,12 @@ func Init() *echo.Echo {
 		api.GET("/test", controller.GetTest())
 		api.GET("/users", controller.GetUser())
 	}
+	auth := e.Group("/auth")
+	{
+		auth.GET("/login", controller.Login())
+		// auth.GET("/logout", controller.Logout())
+		// auth.GET("/signup", controller.Signout())
+	}
 
 	
 	return e
