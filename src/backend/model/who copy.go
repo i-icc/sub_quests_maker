83722442ab80ebee58d492a716bc50c1b28ee3a0
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-05-24 13:32:15 +0900

    add model

---
+++ src/backend/model/who copy.go
@@ -0,0 +1,12 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Timing struct {
+	gorm.Model
+	//`json:"--"`はリクエストボディに指定する名前
+	Id          string `json:"id" xml:"id"`
+	Instruction string `json:"instruction" xml:"instruction"`
+}
